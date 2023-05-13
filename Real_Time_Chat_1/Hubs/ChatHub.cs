5bacfde3f103eb807e0f080d684a0eba832a8b91
lesonhai0339 <haileds939@gmail.com>
2023-05-14 02:03:52 +0700

    Add project files.

---
+++ Real_Time_Chat_1/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Real_Time_Chat1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            await Clients.All.SendAsync("ReceiMessage",name, message);
+        }
+    }
+}
---
+++ Real_Time_Chat_1/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/ChatHub").build();
+
+connection.on("ReceiMessage", function (name, message) {
+    var msg = name + ": " + message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#list").prepend(li);
+});
+connection.start();
+$("#btnSend").on("click", function () {
+    var name = $("#txtUser").val();
+    var message = $("#txtMsg").val();
+
+    connection.invoke("SendMessage", name, message);
+});
